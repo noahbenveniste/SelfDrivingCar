87109ef81fbf57fea6afa1f22b07c8815618c3d4
Noah Benveniste <benven.noa1995@gmail.com>
2019-09-10 20:59:47 -0400

    Added base blueprint and C++ classes for the car, player and AI controllers, game mode, etc.

---
+++ SelfDrivingCar/Source/SelfDrivingCar/CarMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/NavMovementComponent.h"
+#include "CarMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SELFDRIVINGCAR_API UCarMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+};
